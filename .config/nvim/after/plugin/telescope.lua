295691b83032d0b9a9d77fc2e6b51b5725bd9d27
Gia Chau <to.the.lettr@pm.me>
2024-03-31 02:31:20 +0700

    add key mapping

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>gw', builtin.grep_string, {})
