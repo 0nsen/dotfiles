baf7b98d52f160b8a00a316d8f96faadcf5f41d0
Gia Chau <to.the.lettr@pm.me>
2024-04-07 17:32:01 +0700

    set up LSP

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,29 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.preset('recommended')
-lsp_zero.setup()
-
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    })
+})
+
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'eslint',
+        'tsserver',
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
+})
