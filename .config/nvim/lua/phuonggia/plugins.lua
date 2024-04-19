c9f810fcae6446f61792c13dd3901e0d7b2e7d3f
Gia Chau <to.the.lettr@pm.me>
2024-04-19 19:41:45 +0700

    added auto-session

--- .config/nvim/lua/phuonggia/plugins.lua
+++ .config/nvim/lua/phuonggia/plugins.lua
@@ -56,4 +56,15 @@ return require('packer').startup(function(use)
     }
 
     use 'lewis6991/gitsigns.nvim'
+
+    use {
+        'rmagatti/auto-session',
+        config = function()
+            require("auto-session").setup {
+                log_level = "error",
+                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+                auto_session_enable_last_session = true,
+            }
+        end
+}
 end)
