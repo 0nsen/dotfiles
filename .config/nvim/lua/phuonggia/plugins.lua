5dfe4485999c99a24daeaf1d3bc0dee7ef88e015
Gia Chau <to.the.lettr@pm.me>
2024-04-20 00:43:34 +0700

    separated auto-session config

--- .config/nvim/lua/phuonggia/plugins.lua
+++ .config/nvim/lua/phuonggia/plugins.lua
@@ -57,14 +57,5 @@ return require('packer').startup(function(use)
 
     use 'lewis6991/gitsigns.nvim'
 
-    use {
-        'rmagatti/auto-session',
-        config = function()
-            require("auto-session").setup {
-                log_level = "error",
-                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-                auto_session_enable_last_session = true,
-            }
-        end
-}
+    use 'rmagatti/auto-session'
 end)
