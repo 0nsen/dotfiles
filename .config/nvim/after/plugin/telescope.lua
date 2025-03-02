98234ed2192e8b8dbdf0bcccee5f01b0ee5c8db1
0nsen <to.the.lettr@pm.me>
2025-03-02 20:50:40 +0700

    keymap telescope find symbols

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,3 +5,5 @@ vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gw', builtin.grep_string, {})
+
+vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {})
