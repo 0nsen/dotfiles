7a3fd3d38e2541cb538593731aa0c7ea56416019
0nsen <to.the.lettr@pm.me>
2025-03-02 20:50:17 +0700

    nvim navigation improve

--- .config/nvim/lua/phuonggia/remap.lua
+++ .config/nvim/lua/phuonggia/remap.lua
@@ -9,3 +9,9 @@ vim.keymap.set('n', '<C-l>', '<C-w>l')
 -- Switch jumplist cursor motion
 vim.keymap.set('n', '<C-o>', '<C-i>')
 vim.keymap.set('n', '<C-i>', '<C-o>')
+
+-- Center the cursor when moving up down
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-b>', '<C-b>zz')
+vim.keymap.set('n', '<C-f>', '<C-f>zz')
