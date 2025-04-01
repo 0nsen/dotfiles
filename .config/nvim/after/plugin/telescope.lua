052ed6886733f5573a7d475689c813af6bffe5c1
0nsen <to.the.lettr@pm.me>
2025-04-01 10:58:01 +0700

    add telescope keybind

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,5 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -7,3 +9,13 @@ vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gw', builtin.grep_string, {})
 
 vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {})
+
+telescope.setup({ -- SAVE SELECTED ITEMS (VIA TAB) TO QUICKFIX LIST
+	defaults = {
+		mappings = {
+			n = {
+				['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist
+			}
+		}
+	}
+})
