375dfb4e5d499ef5107095492e9846ff6485e99a
0nsen <to.the.lettr@pm.me>
2025-06-26 22:43:47 +0700

    add telescope keybind, ui layout

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -7,15 +7,13 @@ vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gw', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>bg', builtin.current_buffer_fuzzy_find, {})
 
 vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, {})
 
-telescope.setup({ -- SAVE SELECTED ITEMS (VIA TAB) TO QUICKFIX LIST
+
+telescope.setup {
 	defaults = {
-		mappings = {
-			n = {
-				['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist
-			}
-		}
-	}
-})
+		layout_strategy = 'flex',
+	},
+}
